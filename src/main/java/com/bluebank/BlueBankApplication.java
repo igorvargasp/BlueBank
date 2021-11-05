f257b215bc6cc736ab5dfa486d0b62b37c407489
Pedro Lima <pedrojppb@gmail.com>
2021-11-04 22:22:11 -0300

    Testes de Unidade e Integração para Cliente

--- src/main/java/com/bluebank/BlueBankApplication.java
+++ src/main/java/com/bluebank/BlueBankApplication.java
@@ -1,6 +1,7 @@
 package com.bluebank;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -38,24 +39,55 @@ public class BlueBankApplication implements CommandLineRunner {
 				.limiteCredito(1000.00)
 				.tipoConta("PF")
 				.build();
-
-		LocalDate date1 = LocalDate.of(2000, 10, 10);
+		
+		Conta conta2= Conta.builder()
+				.id(null)
+				.conta(88888L)
+				.agencia(777L)
+				.saldo(700.00)
+				.limiteCredito(1000.00)
+				.tipoConta("PF")
+				.build();
+		
+		Conta conta3 = Conta.builder()
+				.id(null)
+				.conta(88888L)
+				.agencia(777L)
+				.saldo(700.00)
+				.limiteCredito(1000.00)
+				.tipoConta("PF")
+				.build();
 
 		Cliente cliente = Cliente.builder()
 				.id(null)
 				.cpf("12312345678")
 				.nomeCompleto("Joao da Silva")
-				.dataNascimento(date1)
+				.dataNascimento(LocalDate.of(2000, 10, 10))
 				.tipoCliente("PF")
 				.email("joao@gmail.com")
 				.telefone("(88)987541236")
 				.rendaMensal(3570.00)
 				.build();
 		
+		Cliente cliente2 = Cliente.builder()
+				.id(null)
+				.cpf("12312345678")
+				.nomeCompleto("Marcos Vinicius")
+				.dataNascimento(LocalDate.of(1998, 10, 10))
+				.tipoCliente("PF")
+				.email("marcos@gmail.com")
+				.telefone("(88)987541236")
+				.rendaMensal(3570.00)
+				.build();
+		
 		conta.setCliente(cliente);
+		conta2.setCliente(cliente);
+		conta3.setCliente(cliente2);
 		cliente.getContas().add(conta);
-		clienteRepository.save(cliente);
-		contaRepository.save(conta);
+		cliente.getContas().add(conta2);
+		cliente.getContas().add(conta3);
+		clienteRepository.saveAll(Arrays.asList(cliente, cliente2));
+		contaRepository.saveAll(Arrays.asList(conta, conta2, conta3));
 		
 		
 	}
