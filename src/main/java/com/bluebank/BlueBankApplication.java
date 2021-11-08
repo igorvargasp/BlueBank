e1e0dea254c6c53810fb22a5ea6df8dbdd8dec94
Pedro Lima <pedrojppb@gmail.com>
2021-11-08 19:34:45 -0300

    ajustes nos testes e script

--- src/main/java/com/bluebank/BlueBankApplication.java
+++ src/main/java/com/bluebank/BlueBankApplication.java
@@ -1,116 +1,14 @@
 package com.bluebank;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.bluebank.entities.Cliente;
-import com.bluebank.entities.Conta;
-import com.bluebank.entities.enums.StatusConta;
-import com.bluebank.entities.enums.TipoCliente;
-import com.bluebank.entities.enums.TipoConta;
-import com.bluebank.entities.enums.TipoTransacao;
-import com.bluebank.repository.ClienteRepository;
-import com.bluebank.repository.ContaRepository;
-import com.bluebank.service.ContaService;
-
 @SpringBootApplication
-public class BlueBankApplication implements CommandLineRunner {
-
-	@Autowired
-	private ClienteRepository clienteRepository;
-
-	@Autowired
-	private ContaRepository contaRepository;
-	
-	@Autowired
-	private ContaService contaService;
+public class BlueBankApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(BlueBankApplication.class, args);
 
 	}
-
-	@Override
-	public void run(String... args) {
-
-		/*
-		Cliente cliente = Cliente.builder()
-				.id(null)
-				.cpf("12312345678")
-				.nomeCompleto("Joao da Silva")
-				.dataNascimento(LocalDate.of(2000, 10, 10))
-				.tipo(TipoCliente.PF.getCod())
-				.email("joao@gmail.com")
-				.telefone("(88)987541236")
-				.rendaMensal(3570.00)
-				.criadoEm(Instant.now())
-				.build();
-		
-		Cliente cliente2 = Cliente.builder()
-				.id(null)
-				.cpf("12312345678")
-				.nomeCompleto("Marcos Vinicius")
-				.dataNascimento(LocalDate.of(1998, 10, 10))
-				.tipo(TipoCliente.PF.getCod())
-				.email("marcos@gmail.com")
-				.telefone("(88)987541236")
-				.rendaMensal(3570.00)
-				.criadoEm(Instant.now())
-				.build();
-		
-		Conta conta = Conta.builder()
-				.id(null)
-				.conta("100001")
-				.agencia("101")
-				.saldo(700.00)
-				.limiteCredito(1000.00)
-				.tipo(TipoConta.CORRENTE.getCod())
-				.status(StatusConta.ATIVA.getCod())
-				.cliente(cliente)
-				.criadoEm(Instant.now())
-				.build();
-		
-		Conta conta2= Conta.builder()
-				.id(null)
-				.conta("20002")
-				.agencia("202")
-				.saldo(800.00)
-				.limiteCredito(2000.00)
-				.tipo(TipoConta.CORRENTE.getCod())
-				.status(StatusConta.ATIVA.getCod())
-				.cliente(cliente)
-				.criadoEm(Instant.now())
-				.build();
-		
-		Conta conta3 = Conta.builder()
-				.id(null)
-				.conta("30003")
-				.agencia("303")
-				.saldo(900.00)
-				.limiteCredito(3000.00)
-				.tipo(TipoConta.CORRENTE.getCod())
-				.status(StatusConta.ATIVA.getCod())
-				.cliente(cliente2)
-				.criadoEm(Instant.now())
-				.build();
-		
-		cliente.getContas().add(conta);
-		cliente.getContas().add(conta2);
-		cliente.getContas().add(conta3);
-		clienteRepository.saveAll(Arrays.asList(cliente, cliente2));
-		contaRepository.saveAll(Arrays.asList(conta, conta2, conta3));
-		
-		contaService.transferFunds(conta2.getId(), conta.getId(), 1000.00, TipoTransacao.PIX.getCod());
-		*/
-	}
-
-
-
 }
--- src/test/java/com/bluebank/resources/ClienteResourceTests.java
+++ src/test/java/com/bluebank/resources/ClienteResourceTests.java
@@ -1,14 +1,8 @@
 package com.bluebank.resources;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,19 +23,14 @@ import com.bluebank.dto.ClienteDTO;
 import com.bluebank.repository.ClienteRepository;
 import com.bluebank.repository.ContaRepository;
 import com.bluebank.service.ClienteService;
-import com.bluebank.service.exceptions.DatabaseException;
 import com.bluebank.service.exceptions.ResourceNotFoundException;
 import com.bluebank.tests.Factory;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(ClienteController.class)
 public class ClienteResourceTests {
 
 	@Autowired
 	private MockMvc mockMvc;
-
-	@Autowired
-	private ObjectMapper objectMapper;
 
 	@MockBean
 	private ClienteService clienteService;
@@ -54,7 +43,6 @@ public class ClienteResourceTests {
 
 	private Long existingId;
 	private Long nonExistingId;
-	private Long dependentId;
 	private ClienteDTO clienteDTO;
 	private PageImpl<ClienteDTO> page;
 
@@ -63,7 +51,6 @@ public class ClienteResourceTests {
 
 		existingId = 1l;
 		nonExistingId = 2l;
-		dependentId = 3L;
 
 		clienteDTO = Factory.createClienteDTO();
 		page = new PageImpl<>(List.of(clienteDTO));
@@ -73,68 +60,7 @@ public class ClienteResourceTests {
 		when(clienteService.findById(existingId)).thenReturn(clienteDTO);
 		when(clienteService.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
 
-		when(clienteService.update(eq(existingId), any())).thenReturn(clienteDTO);
-		when(clienteService.update(eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
-
-		doNothing().when(clienteService).delete(existingId);
-		doThrow(ResourceNotFoundException.class).when(clienteService).delete(nonExistingId);
-		doThrow(DatabaseException.class).when(clienteService).delete(dependentId);
-
 		when(clienteService.insert(clienteDTO)).thenReturn(clienteDTO);
-	}
-
-	@Test
-	public void deleteShouldDeleteResourceWhenIdExists() throws Exception {
-
-		ResultActions result = mockMvc.perform(delete("/clientes/{id}", existingId).accept(MediaType.APPLICATION_JSON));
-
-		result.andExpect(status().isNoContent());
-	}
-
-	@Test
-	public void deleteShouldThrowResourceNotFoundException() throws Exception {
-
-		ResultActions result = mockMvc.perform(delete("/clientes/{id}", nonExistingId).accept(MediaType.APPLICATION_JSON));
-
-		result.andExpect(status().isNotFound());
-	}
-
-	@Test
-	public void insertShouldReturnClienteDTO() throws Exception {
-
-		String jsonBody = objectMapper.writeValueAsString(clienteDTO);
-
-		ResultActions result = mockMvc.perform(post("/clientes").content(jsonBody)
-				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON));
-
-		result.andExpect(status().isCreated());
-		result.andExpect(jsonPath("$.nomeCompleto").exists());
-		result.andExpect(jsonPath("$.email").exists());
-	}
-
-	@Test
-	public void updateShouldReturnClienteDTOWhenIdExists() throws Exception {
-
-		String jsonBody = objectMapper.writeValueAsString(clienteDTO);
-
-		ResultActions result = mockMvc.perform(put("/clientes/{id}", existingId).content(jsonBody)
-				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON));
-
-		result.andExpect(status().isOk());
-		result.andExpect(jsonPath("$.nomeCompleto").exists());
-		result.andExpect(jsonPath("$.email").exists());
-		result.andExpect(jsonPath("$.dataNascimento").exists());
-	}
-
-	@Test
-	public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
-
-		String jsonBody = objectMapper.writeValueAsString(clienteDTO);
-
-		ResultActions result = mockMvc.perform(put("/clientes/{id}", nonExistingId).content(jsonBody)
-				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON));
-
-		result.andExpect(status().isNotFound());
 	}
 
 	@Test
